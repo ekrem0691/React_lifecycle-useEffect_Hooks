d90d81a469f7f600574e26415007948043abf376
ekrem0691 <06hilalekrem06@gmail.com>
2022-08-23 12:50:54 +0300

    notes added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import LifeCycleMethods from './components/LifeCycleMethods';
-import { useState } from 'react';
-import UseEffectHook from './components/UseEffectHook';
-import Users from './components/Users';
+import { useEffect, useState } from "react";
+import LifeCycleMethods from "./components/LifeCycleMethods";
+import UseEffectHook from "./components/UseEffectHook"
+
 
 function App() {
+
+
   const [show, setShow] = useState(true);
+  console.log(show)
+
 
   return (
     <div className="container text-center" style={{ marginTop: '10rem' }}>
-      {/* <button className="btn btn-dark" onClick={() => setShow(!show)}>
-        {show ? 'Hide' : 'Show'}
-      </button> */}
-
-      {/* Conditional Rendering (Short-Circuit) */}
-      {/* {show && <LifeCycleMethods />} */}
-      {/* {show ? <LifeCycleMethods /> : null} */}
-      {/* {show && <UseEffectHook/>} */}
-      <Users />
+      <button className="btn btn-dark" onClick={() => setShow(!show)} >{ show ? "Hide" : "Show" }</button>
+      
+      {/* Conditional Renderin (Short-Circuit) */}
+      {show && <LifeCycleMethods /> }
+      {/* {show ? <LifeCycleMethods /> : null } */}
+      {/* {show && <UseEffectHook/> } */}
+     
     </div>
   );
 }
 
 export default App;
+
